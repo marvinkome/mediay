7e992fefbcf84cb79f34afcebd03daa283fda943
rooseveltAI <howdy.roosevelt@gmail.com>
2022-10-24 11:40:31 +0100

    add mobile site

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,26 +11,6 @@ import {
 } from '@chakra-ui/react'
 import SignupModal from 'components/auth'
 
-const StartButton = (props: any) => {
-  return (
-    <SignupModal>
-      <Button
-        bgColor={props.bg}
-        color={props.color}
-        border={props.border}
-        rounded="full"
-        fontSize={{ base: '', md: '16px' }}
-        size="md"
-        px={{ base: '', md: '2rem' }}
-        py={{ base: '', md: '1.8rem' }}
-        alignSelf="flex-start"
-      >
-        Get Started
-      </Button>
-    </SignupModal>
-  )
-}
-
 const Page = () => {
   return (
     <>
@@ -55,11 +35,16 @@ const Page = () => {
           </SignupModal>
         </Stack>
       </chakra.nav>
-      <Container maxW="100%" py="0" h="95vh" overflow="hidden">
+      <Container
+        maxW="100%"
+        p="0"
+        h={{ base: '90vh', md: '95vh' }}
+        overflow="hidden"
+      >
         <Stack
           px={{ base: '12px', md: '8rem' }}
           pb={{ base: '16px', md: '6rem' }}
-          pt={{ base: '16px', md: '6rem' }}
+          pt={{ base: '3rem', md: '6rem' }}
         >
           <Heading
             as="h1"
@@ -84,20 +69,21 @@ const Page = () => {
           bgSize="100%"
           // objectFit="cover"
           bgPos="center"
-          px={{ base: '', md: '10rem' }}
-          direction={{ base: 'column', md: 'row' }}
+          px={{ base: '12px', md: '8rem' }}
+          pt={{ base: '16px', md: '0rem' }}
+          direction={{ base: 'column-reverse', md: 'row' }}
+          spacing={{ base: '8', md: '0' }}
         >
           <Image
             src="images/hero_image_mediay.png"
             alt="mediay-app"
-            boxSize="85%"
+            boxSize={{ base: '100%', md: '85%' }}
           />
           <Heading
-            w="325px"
-            fontFamily="General Sans"
+            w={{ base: '90%', md: '325px' }}
             fontWeight="500"
-            fontSize="48px"
-            lineHeight="65px"
+            fontSize={{ base: '32px', md: '48px' }}
+            lineHeight={{ base: '43px', md: '65px' }}
             textTransform="capitalize"
             opacity=" 0.98"
             position="relative"
@@ -115,53 +101,57 @@ const Page = () => {
           p="0"
           bgImage="url(/images/bg_section_2.png)"
           bgRepeat="no-repeat"
-          bgSize="100%"
-          // objectFit="cover"
+          bgSize="cover"
           bgPos="center"
-          // overflow="hidden"
-          // h="80vh"
+
+          // objectFit="cover"
         >
           <Stack
             direction={{ base: 'column', md: 'row' }}
             spacing="0"
             align="center"
             justify="center"
+            py={{ base: '12', md: '0' }}
+            // spac
           >
             <Stack
+              direction={{ base: 'column', md: 'column' }}
               position="relative"
-              right={{ base: '', md: '-16rem' }}
-              spacing={{ base: '', md: '6' }}
+              right={{ base: '0', md: '-16rem' }}
+              spacing={{ base: '2', md: '6' }}
+              px={{ base: '4', md: '0' }}
             >
               <Image
                 src="images/create_groups.svg"
                 alt="Create Groups"
-                boxSize="90%"
-                alignSelf="flex-start"
+                boxSize={{ base: '50%', md: '90%' }}
+                alignSelf={{ base: 'flex-start', md: 'flex-start' }}
               />
               <Image
                 src="images/share_subscriptions.svg"
                 alt="Share Subscriptions"
-                boxSize="100%"
+                alignSelf={{ base: 'center', md: 'flex-start' }}
+                boxSize={{ base: '70%', md: '100%' }}
               />
               <Image
                 src="images/split_bills.svg"
                 alt="Split Bills"
-                boxSize="80%"
-                alignSelf="flex-end"
+                boxSize={{ base: '40%', md: '80%' }}
+                alignSelf={{ base: 'center', md: 'flex-end' }}
               />
             </Stack>
             <Stack
               flex="1"
-              pt={{ base: '', md: '4rem' }}
-              pb={{ base: '', md: '4rem' }}
-              pl={{ base: '', md: '4rem' }}
+              pt={{ base: '1rem', md: '4rem' }}
+              pb={{ base: '1rem', md: '4rem' }}
+              pl={{ base: '1rem', md: '4rem' }}
               align="flex-end"
-              pr={{ base: '', md: '4rem' }}
+              pr={{ base: '0', md: '4rem' }}
             >
               <Image
                 src="images/hero_image_mediay.png"
                 alt="mediay-app"
-                boxSize="90%"
+                boxSize={{ base: '100%', md: '90%' }}
               />
             </Stack>
           </Stack>
@@ -169,12 +159,12 @@ const Page = () => {
             <Image
               src="images/love_you_gesture.png"
               alt="love-you-gesture"
-              boxSize="32%"
+              boxSize={{ base: '50%', md: '32%' }}
               objectFit="contain"
               alignSelf="flex-end"
               pos="absolute"
-              right={{ base: '', md: '2rem' }}
-              bottom={{ base: '', md: '-2rem' }}
+              right={{ base: '0', md: '2rem' }}
+              bottom={{ base: '-4rem', md: '-2rem' }}
             />
           </Stack>
         </Container>
@@ -189,29 +179,30 @@ const Page = () => {
           <Stack
             justify="center"
             align="center"
-            spacing={{ base: '', md: '-6rem' }}
+            spacing={{ base: '-2rem', md: '-6rem' }}
+            py={{ base: '2rem', md: '1rem' }}
           >
-            <Stack direction="row" spacing={{ base: '', md: '12' }}>
+            <Stack direction="row" spacing={{ base: '8', md: '12' }}>
               <Text
-                fontSize={{ base: '', md: '100px' }}
+                fontSize={{ base: '32px', md: '100px' }}
                 fontWeight="700"
-                lineHeight={{ base: '', md: '136px' }}
+                lineHeight={{ base: '43px', md: '136px' }}
                 textTransform="uppercase"
               >
                 Before
               </Text>
               <Text
-                fontSize={{ base: '', md: '100px' }}
+                fontSize={{ base: '32px', md: '100px' }}
                 fontWeight="700"
-                lineHeight={{ base: '', md: '136px' }}
+                lineHeight={{ base: '43px', md: '136px' }}
                 textTransform="uppercase"
               >
                 &
               </Text>
               <Text
-                fontSize={{ base: '', md: '100px' }}
+                fontSize={{ base: '32px', md: '100px' }}
                 fontWeight="700"
-                lineHeight={{ base: '', md: '136px' }}
+                lineHeight={{ base: '43px', md: '136px' }}
                 color="#D2FC04"
                 textTransform="uppercase"
               >
@@ -220,9 +211,9 @@ const Page = () => {
             </Stack>
             <Heading
               color="#FA5B17"
-              fontSize={{ base: '', md: '260px' }}
+              fontSize={{ base: '92px', md: '260px' }}
               fontWeight="700"
-              lineHeight={{ base: '', md: '351px' }}
+              lineHeight={{ base: '124px', md: '351px' }}
             >
               Mediay
             </Heading>
@@ -234,6 +225,14 @@ const Page = () => {
               alt="Before and After Mediay"
               boxSize="100%"
               objectFit="contain"
+              display={{ base: 'none', md: 'block' }}
+            />
+            <Image
+              src="images/b_a_sm.svg"
+              alt="Before and After Mediay"
+              boxSize="100%"
+              objectFit="contain"
+              display={{ base: 'block', md: 'none' }}
             />
           </Stack>
         </Container>
@@ -242,9 +241,11 @@ const Page = () => {
       <chakra.section>
         <Container maxW="100%" p="0">
           <Stack
+            w="100%"
+            overflow="hidden"
             direction="row"
             align="center"
-            spacing={{ base: '', md: '12' }}
+            spacing={{ base: '8', md: '12' }}
           >
             <Heading
               fontStyle="normal"
@@ -290,6 +291,8 @@ const Page = () => {
             </Heading>
           </Stack>
           <Stack
+            w="100%"
+            overflow="hidden"
             direction="row"
             align="center"
             spacing={{ base: '', md: '12' }}
@@ -339,18 +342,18 @@ const Page = () => {
           </Stack>
         </Container>
 
-        <chakra.aside h={{ base: '', md: '50vh' }}>
-          <Stack align="center" justify="center" h="100%">
+        <chakra.aside h={{ base: '40vh', md: '50vh' }}>
+          <Stack align="center" w="100%" justify="center" h="100%">
             <Button
               fontWeight="700"
-              fontSize=" 32px"
+              fontSize={{ base: '28px', md: '32px' }}
               lineHeight=" 43px"
               textTransform="capitalize"
               bgColor="#E70808"
               color="rgba(255, 255, 255, 0.87)"
               size="lg"
-              px={{ base: '', md: '10rem' }}
-              py={{ base: '', md: '2.4rem' }}
+              px={{ base: '6rem', md: '10rem' }}
+              py={{ base: '2rem', md: '2.4rem' }}
               rounded="12px"
               alignSelf="center"
             >
@@ -364,162 +367,86 @@ const Page = () => {
         <Container
           maxW="100%"
           p="0"
-          h="10vh"
+          h="19vh"
           bgImage="url(/images/bg_footer.png)"
           bgRepeat="no-repeat"
           bgSize="100%"
           bgPos="center"
         >
           <Grid
-            templateColumns={{ base: '', md: 'repeat(1, 1fr)' }}
-            templateRows={{ base: '', md: 'repeat(1, 1fr)' }}
-            rowGap={{ base: '', md: '1rem' }}
             overflow="hidden"
+            pb={{ base: '8', md: '8' }}
+            templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(3, 1fr)' }}
           >
-            <Stack>
-              <Grid templateColumns={{ base: '', md: 'repeat(3, 1fr)' }}>
-                <Stack
-                  justify="space-between"
-                  h="100%"
-                  align="left"
-                  gridColumn="1 / 3"
-                  // w="58%"
-                  // pr={{ base: '', md: '8' }}
+            <Stack
+              justify="space-between"
+              h="100%"
+              align="left"
+              gridColumn={{ base: '1 / 1', md: '1 / 3' }}
+            >
+              <Text
+                fontSize={{ base: '28px', md: '68px' }}
+                fontWeight="700"
+                lineHeight={{ base: '36px', md: '68px' }}
+              >
+                In the future, everyone will be subscribed to a service for
+                convenience and FOMO.
+              </Text>
+              <Stack
+                w={{ base: '30%', md: '40%' }}
+                align="center"
+                p={{ base: '0', md: '24px' }}
+              >
+                <Text
+                  textTransform="uppercase"
+                  opacity="68%"
+                  fontSize={{ base: '10px', md: '13px' }}
+                  fontWeight="400"
+                  letterSpacing="0.08em"
+                  whiteSpace="nowrap"
                 >
-                  <Text
-                    fontSize={{ base: '', md: '68px' }}
-                    fontWeight="700"
-                    lineHeight={{ base: '', md: '68px' }}
-                  >
-                    In the future, everyone will be subscribed to a service for
-                    convenience and FOMO.
-                  </Text>
-                  {/* <Image src="/logo.svg" alt="logo" boxSize="100px" p="4" /> */}
-                </Stack>
-
-                <Stack
-                  justify="flex-start"
-                  align="flex-end"
-                  pt={{ base: '', md: '1rem' }}
-                >
-                  <Stack
-                    transform="rotate(-90deg)"
-                    spacing={{ base: '', md: '-2.8rem' }}
-                  >
-                    <Heading
-                      as="h1"
-                      fontStyle="normal"
-                      fontWeight="700"
-                      fontSize={{ base: '80px', md: '126px' }}
-                      whiteSpace="nowrap"
-                      lineHeight={{ base: '188px', md: '197px' }}
-                      textTransform="uppercase"
-                    >
-                      Mediay
-                    </Heading>
-                    <Heading
-                      as="h1"
-                      fontStyle="normal"
-                      fontWeight="700"
-                      fontSize={{ base: '80px', md: '126px' }}
-                      whiteSpace="nowrap"
-                      color="transparent"
-                      lineHeight={{ base: '188px', md: '110px' }}
-                      textTransform="uppercase"
-                      css={{
-                        '-webkit-text-stroke': '2px white',
-                        'text-stroke': '2px white'
-                      }}
-                    >
-                      Mediay
-                      <br /> Mediay
-                      <br /> Mediay
-                    </Heading>
-                  </Stack>
-                </Stack>
-              </Grid>
+                  © {new Date().getFullYear()} Mediay.
+                </Text>
+              </Stack>
             </Stack>
 
             <Stack
-              overflow="hidden"
-              w="100%"
-              align="center"
-              justify="flex-end"
-              spacing="2"
-              p={{ base: '', md: '24px' }}
+              justify="flex-start"
+              align="flex-end"
+              pt={{ base: '1rem', md: '1rem' }}
             >
-              <Stack direction="row">
-                <StartButton
-                  border="1px solid #D2FC04"
-                  bg="transparent"
-                  color="#D2FC04"
-                />
-                <StartButton bg="#D2FC04" color="#000" />
-                <StartButton
-                  border="1px solid #fff"
-                  bg="transparent"
-                  color="#fff"
-                />
-                <StartButton bg="#D2FC04" color="#000" />
-                <StartButton bg="#D2FC04" color="#000" />
-                <StartButton
-                  border="1px solid #D2FC04"
-                  bg="transparent"
-                  color="#D2FC04"
-                />
-                <StartButton bg="#D2FC04" color="#000" />
-                <StartButton bg="#D2FC04" color="#000" />
-                <StartButton
-                  border="1px solid #fff"
-                  bg="transparent"
-                  color="#fff"
-                />
-              </Stack>
-              <Stack direction="row">
-                <StartButton bg="#D2FC04" color="#000" />
-
-                <StartButton
-                  border="1px solid #EB72FF"
-                  bg="transparent"
-                  color="#EB72FF"
-                />
-                <StartButton
-                  border="1px solid #fff"
-                  bg="transparent"
-                  color="#fff"
-                />
-
-                <StartButton
-                  border="1px solid #D2FC04"
-                  bg="transparent"
-                  color="#D2FC04"
-                />
-                <StartButton bg="#D2FC04" color="#000" />
-                <StartButton
-                  border="1px solid #D2FC04"
-                  bg="transparent"
-                  color="#D2FC04"
-                />
-
-                <StartButton bg="#D2FC04" color="#000" />
-                <StartButton
-                  border="1px solid #EB72FF"
-                  bg="transparent"
-                  color="#EB72FF"
-                />
-                <StartButton bg="#D2FC04" color="#000" />
-
-                <StartButton
-                  border="1px solid #fff"
-                  bg="transparent"
-                  color="#fff"
-                />
-                <StartButton bg="#D2FC04" color="#000" />
-                <StartButton
-                  border="1px solid #D2FC04"
-                  bg="transparent"
-                  color="#D2FC04"
-                />
+              <Stack
+                transform="rotate(-90deg)"
+                spacing={{ base: '-0.2rem', md: '-0.8rem' }}
+              >
+                <Heading
+                  as="h1"
+                  fontStyle="normal"
+                  fontWeight="700"
+                  fontSize={{ base: '62px', md: '80px' }}
+                  whiteSpace="nowrap"
+                  lineHeight={{ base: '58px', md: '96px' }}
+                  textTransform="uppercase"
+                >
+                  Mediay
+                </Heading>
+                <Heading
+                  fontStyle="normal"
+                  fontWeight="700"
+                  fontSize={{ base: '62px', md: '80px' }}
+                  whiteSpace="nowrap"
+                  color="transparent"
+                  lineHeight={{ base: '58px', md: '68px' }}
+                  textTransform="uppercase"
+                  css={{
+                    '-webkit-text-stroke': '2px white',
+                    'text-stroke': '2px white'
+                  }}
+                >
+                  Mediay
+                  <br /> Mediay
+                  <br /> Mediay
+                </Heading>
               </Stack>
             </Stack>
           </Grid>
